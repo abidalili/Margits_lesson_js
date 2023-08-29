16f137f508af9321474f84d6251f6529989ead3d
abidalili <abidalili1995@gmail.com>
2023-08-29 14:18:54 +0300

    javascript applied

--- task-5/main.js
+++ task-5/main.js
@@ -1,4 +1,13 @@
-const price = document.querySelector("#price");
+const calcGasoline = () => {
+  const price = document.querySelector("#price");
 
-const money = document.getElementById("#money");
-console.log(price.value);
+  const money = document.querySelector("#money");
+  const answer = document.querySelector("#answer");
+  let amount = (money.value / price.value).toFixed(2);
+  console.log(amount);
+  let text;
+  amount >= 10
+    ? (text = `You could get about ${amount} liters, you can escape`)
+    : `You could get about ${amount} liters, sorry you have to stay`;
+  answer.textContent = text;
+};
